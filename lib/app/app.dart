f4ad5660949392fe5bfb550e2978a0d630f7a11d
abualhamd <141117351+abualhamd-tot@users.noreply.github.com>
2023-12-19 20:10:35 +0200

    refactored app to use flutter_hooks

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,21 +1,16 @@
 import 'package:bmi_calculator_2/app/utils/app_strings.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../view/provider/bmi_provider.dart';
 import 'config/app_theme.dart';
 import 'config/go_router.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      child: MaterialApp.router(
-        routerConfig: appRouter,
-        title: AppStrings.appTitle,
-        theme: appTheme,
-      ),
-      create: (context) => BMIProvider(),
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      title: AppStrings.appTitle,
+      theme: appTheme,
     );
   }
 }
--- lib/view/screens/calculator_view/calculator_screen.dart
+++ lib/view/screens/calculator_view/calculator_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
 import '../../../app/config/go_router.dart';
 import '../../../app/utils/app_strings.dart';
@@ -8,17 +8,43 @@ import '../../../app/utils/colors_manager.dart';
 import '../../../app/utils/enums/enums.dart';
 import '../../components/count_widget.dart';
 import '../../components/material_pink_button.dart';
-import '../../provider/bmi_provider.dart';
 import 'components/gender_row_widget.dart';
 import 'components/height_slider_widget.dart';
 
-class BMICalcuator extends StatelessWidget {
+class BMICalcuator extends HookWidget {
   const BMICalcuator();
 
   @override
   Widget build(BuildContext context) {
-    final read = context.read<BMIProvider>();
-    final watch = context.watch<BMIProvider>();
+    int calcBMI({required int weight, required int height}) {
+      final value = weight / ((height / 100) * (height / 100));
+      return value.toInt();
+    }
+
+    String getInterpretation({required int bmiIndex}) {
+      if (bmiIndex >= 25) {
+        return AppStrings.overweight;
+      } else if (bmiIndex >= 18) {
+        return AppStrings.normal;
+      } else {
+        return AppStrings.underweight;
+      }
+    }
+
+    String getAdvice({required int bmiIndex}) {
+      if (bmiIndex >= 25) {
+        return AppStrings.overweightMessage;
+      } else if (bmiIndex >= 18) {
+        return AppStrings.normalMessage;
+      } else {
+        return AppStrings.underweightMessage;
+      }
+    }
+
+    ValueNotifier<Gender?> genderState = useState(null);
+    var height = useState(180);
+    var weight = useState(78);
+    var age = useState(20);
 
     return Scaffold(
       appBar: AppBar(
@@ -36,32 +62,31 @@ class BMICalcuator extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             GenderRowWidget(
-              choosenGender: watch.bmiModel.gender,
+              choosenGender: genderState.value,
               onCardTapped: ({required Gender gender}) {
-                read.setGender(gender: gender);
+                genderState.value = gender;
               },
             ),
             SizedBox(height: 20),
             HeightSlider(
-              height: watch.bmiModel.height,
+              height: height.value,
               onHeightChanged: (value) {
-                read.setHeight(height: value.round().toInt());
+                height.value = value.round().toInt();
               },
             ),
             SizedBox(height: 20),
             //? weight & age count widgets
             Row(
-              // mainAxisAlignment: MainAxisAlignment.,
               children: [
                 Expanded(
                   child: CountWidget(
                     title: AppStrings.weight,
-                    count: watch.bmiModel.weight,
+                    count: weight.value,
                     onIncrease: () {
-                      read.incWeight();
+                      weight.value++;
                     },
                     onDecrease: () {
-                      read.decWeight();
+                      weight.value--;
                     },
                   ),
                 ),
@@ -69,12 +94,12 @@ class BMICalcuator extends StatelessWidget {
                 Expanded(
                   child: CountWidget(
                     title: AppStrings.age,
-                    count: watch.bmiModel.age,
+                    count: age.value,
                     onIncrease: () {
-                      read.incAge();
+                      age.value++;
                     },
                     onDecrease: () {
-                      read.decAge();
+                      age.value--;
                     },
                   ),
                 ),
@@ -85,11 +110,16 @@ class BMICalcuator extends StatelessWidget {
       ),
       bottomNavigationBar: MaterialPinkButton(
         onPressed: () {
-          read.calcBMI();
+          final bmiIndex = calcBMI(
+            weight: weight.value,
+            height: height.value,
+          );
+          final interpretation = getInterpretation(bmiIndex: bmiIndex);
+          final advice = getAdvice(bmiIndex: bmiIndex);
           context.goNamed(AppRoutes.resultsRoute, extra: {
-            "bmiIndex": read.bmiIndex,
-            "interpretation": read.interpretation,
-            "advice": read.advice,
+            "bmiIndex": bmiIndex,
+            "interpretation": interpretation,
+            "advice": advice,
           });
         },
         text: AppStrings.calcBMI,
